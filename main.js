629545f4a377217953e7862ef7434f9afbbf2c3e
saaraOM <106832341+saaraOM@users.noreply.github.com>
2024-09-10 11:25:36 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');
+let tamanhoSenha = 12;
+numeroSenha.textContent = tamanhoSenha;
 
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+console.log(botoes)
+
+function diminuiTamanho(){
+    tamanhoSenha = tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
