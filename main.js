fedc0cc9a1ed3f83b7f74c359858474eb0ab80a3
saaraOM <106832341+saaraOM@users.noreply.github.com>
2024-09-24 11:20:41 -0300

    Update main.js

--- main.js
+++ main.js
@@ -5,8 +5,17 @@ numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 console.log(botoes)
-
+botoes[0].onclick = diminuiTamanho;
+botoes[1].onclick = aumentaTamanho;
 function diminuiTamanho(){
-    tamanhoSenha = tamanhoSenha-1;
+    if (tamanhoSenha > 1){
+        tamanhoSenha = tamanhoSenha-1;
+    }
     numeroSenha.textContent = tamanhoSenha;
+}
+function aumentaTamanho(){
+    if (tamanhoSenha < 20){
+        tamanhoSenha = tamanhoSenha+1;
+     }
+     numeroSenha.textContent = tamanhoSenha;
 }
