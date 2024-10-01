5b42c622eb72c65a35ec41622d6c8a7c54b7d544
saaraOM <106832341+saaraOM@users.noreply.github.com>
2024-10-01 10:40:22 -0300

    Update main.js

--- main.js
+++ main.js
@@ -27,10 +27,11 @@ geraSenha();
 campoSenha.value = letrasMaiusculas;
 
 function geraSenha(){
-  for (let i = 0; i < tamanhoSenha;i++){
-      let senha = '' ”;
-    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
-    (numeroAleatorio);= Math.floor(numeroAleatorio);
+ let senha = '' ”;
+ for (let i = 0; i < tamanhoSenha;i++){
+     let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+     numeroAleatorio;= Math.floor(numeroAleatorio);
     senha = senha + (letrasMaiusculas[numeroAleatorio]);
 }
+    campoSenha.value = senha;
 }
